4b86bb1fe6190a43ff143548c93d738f2d0b42bf
anamez22 <ana.amezquita@correounivalle.edu.co>
2024-10-08 23:03:14 -0500

    Add files via upload

---
+++ ejercicio4labo4.py
@@ -0,0 +1,13 @@
+#ejercicio 4
+class termostato:
+    def _int_(self, temperatura_inicial):
+        self.temperatura = temperatura_inicial
+
+    def aumentar_temperatura(self, grados):
+        self.temperatura += grados 
+
+    def disminuir_temperatura(self, grados):
+        self.temperatura -= grados 
+
+    def mostrar_temperatura(self):
+        print(f"la temperatura actual es {self.temperatura}°c.")
